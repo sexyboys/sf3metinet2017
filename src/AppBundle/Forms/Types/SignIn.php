cc7e9d3f0eebd46de6b19135bf5c6e6464459190
Boris Guéry <guery.b@gmail.com>
2017-03-01 15:44:24 +0100

    fix sign in form

--- src/AppBundle/Forms/Types/SignIn.php
+++ src/AppBundle/Forms/Types/SignIn.php
@@ -7,10 +7,6 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Callback;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 /**
  * @author Boris Guéry <guery.b@gmail.com>
